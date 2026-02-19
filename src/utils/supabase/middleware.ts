0cb3abdb650b4ea3e6cc1455cf299fc29f80b4f3
bakursky <rrrrrrrrush@gmail.com>
2026-02-19 19:34:57 +0300

    fix welcome page redirect bug

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -40,11 +40,12 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    !request.nextUrl.pathname.startsWith('/auth') &&
+    !request.nextUrl.pathname.startsWith('/welcome')
   ) {
-    // no user, potentially respond by redirecting the user to the login page
+    // no user, potentially respond by redirecting the user to the welcome page
     const url = request.nextUrl.clone()
-    url.pathname = '/landing/welcome'
+    url.pathname = '/welcome'
     return NextResponse.redirect(url)
   }
 
